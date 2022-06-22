3d99d603296ff04a37c0bc814af4195b315b816b
Vincent Sheehan <35037321+VincentSheehan22@users.noreply.github.com>
2022-06-22 15:44:13 +0100

    Update investment-return-calculator.py
    Add argument for year-on-year investment.

--- investment-return-calculator.py
+++ investment-return-calculator.py
@@ -1,11 +1,12 @@
 # Investment return calcualtor.
 
-def invest(amount, term, roi):
+def invest(amount, annual, term, roi):
     """Calculate investment value based on initial amount, term and rate of return."""
     for year in range(term):
+        amount = amount + annual
         amount = amount + (amount * roi)
         print(f"Year {year + 1}: €{amount}")
     return amount
 
 
-print(invest(1350, 7, 0.10))
+print(invest(1350, 1800, 7, 0.07))
